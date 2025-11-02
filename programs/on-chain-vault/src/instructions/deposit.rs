2db023e38a9635405e81d48a43b93f1b96b676a0
Christone007 <exellentemy@gmail.com>
2025-11-02 03:31:32 +0000

    Implement the proper account constraints

--- programs/on-chain-vault/src/instructions/deposit.rs
+++ programs/on-chain-vault/src/instructions/deposit.rs
@@ -19,9 +19,11 @@ use crate::events::DepositEvent;
 
 #[derive(Accounts)]
 pub struct Deposit<'info> {
-    pub user: Signer<'info>,
-    pub vault: Account<'info, Vault>,
-    pub system_program: Program<'info, System>
+    #[account(mut)]
+    pub user: Signer<'info>, // mutable because this user is making the transfer to the vault
+    #[account(mut)]
+    pub vault: Account<'info, Vault>, //mutable because the vault balance will change too
+    pub system_program: Program<'info, System> // needed because the user making this transfer may not be the vault owner, hence a CPI is needed
 }
 
 pub fn _deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
