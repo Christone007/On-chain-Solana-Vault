0adc677bd0923808548d455ebdcc22a6abc46d7a
Christone007 <exellentemy@gmail.com>
2025-11-02 03:54:45 +0000

    Invoke the system program transfer instruction

--- programs/on-chain-vault/src/instructions/deposit.rs
+++ programs/on-chain-vault/src/instructions/deposit.rs
@@ -28,13 +28,32 @@ pub struct Deposit<'info> {
 
 pub fn _deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
     // retrieve the user and vault accounts
-    
+    let user = &mut ctx.accounts.user;
+    let vault = &mut ctx.accounts.vault;
 
     // check if the user has more lamports balance
+    require!(user.to_account_info().lamports() >= amount, VaultError::InsufficientBalance);
 
     // verify that the vault is not locked
+    require!(!vault.locked, VaultError::VaultLocked);
 
     // transfer the lamports using CPI to invoke system program
+    // create the transfer instruction (sys prog): it takes the from pubkey, the to pubkey and the amount of lamports
+    let transfer_instruction = transfer(
+        user.key(),
+        vault.key(),
+        amount
+    );
+
+    // invoke the instruction, provide an array of all accounts involved in the transaction placing the signer first
+    invoke(
+        &transfer_instruction,
+        &[
+            user.to_account_info(), // the user signs the transaction
+            vault.to_account_info(),
+            ctx.accounts.system_program.to_account_info()
+        ]
+    )?;
 
     // emit success response
 
